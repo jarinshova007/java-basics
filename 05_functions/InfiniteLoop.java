a8b612fed2ea068b849ca3207f5c542028bdafe6
Khandakar Jarin Shova <jarinshova007@gmail.com>
2025-09-30 01:12:35 +0600

    feat: add files

---
+++ 05_functions/InfiniteLoop.java
@@ -0,0 +1,19 @@
+// Run the loop and try to understand what happens 
+public class InfiniteLoop {
+    public static void main(String[] args) {
+        // infinite for loop
+        for (; ;) {
+            System.out.println("Hello World");
+        }
+        /*
+        infinite while loop
+        while (true) {
+            System.out.println("Hello World");
+        }
+
+        infinite do while loop
+        do {
+            System.out.println("Hello World");
+        } while (true);    */
+    }
+}
---
+++ 05_functions/RunTheLoop.java
@@ -0,0 +1,9 @@
+// Run the loop and try to understand what happens 
+public class ILoop {
+    public static void main(String[] args) {
+        // infinite loop
+        for (; ;) {
+            System.out.println("Hello World");
+        }
+    }
+}
