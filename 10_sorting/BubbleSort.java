0c07e0eea7384681056361d02ab4cac98486ce8e
Khandakar Jarin Shova <jarinshova007@gmail.com>
2025-10-06 06:36:13 +0600

    feat: add sorting folder

---
+++ 10_sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+    public static void printArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {5, 8, 9, 6, 2, 1, 3};
+        
+        // bubble sorting
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
