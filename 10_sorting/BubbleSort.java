94440996751ca542269839fc9de0f6ff2d30ac4b
Khandakar Jarin Shova <jarinshova007@gmail.com>
2025-10-06 17:09:17 +0600

    feat: add InsertionSort file and modified BubbleSort file

--- 10_sorting/BubbleSort.java
+++ 10_sorting/BubbleSort.java
@@ -4,20 +4,23 @@ public class BubbleSort {
             System.out.print(array[i] + " ");
         }
     }
+
     public static void main(String[] args) {
-        int arr[] = { 5, 8, 9, 6, 2, 1, 3 };
+        int arr[] = {5, 8, 19, 6, 12, 1, 3};
 
         // bubble sorting
         // time complexity = O(n^2)
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
+            for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
         }
+        // method call
         printArray(arr);
     }
 }
---
+++ 10_sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+public class InsertionSort {
+    // print array function
+    public static void printArray(int array[]) {
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {3, 8, 15, 13, 4, 1, 6};
+
+        // insertion sorting
+        // time complexity = O()
+        // traverse in unsorted part
+        for (int i = 1; i < arr.length - 1; i++) {
+            int current = arr[i];
+            // traverse in sorted part
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+        // method call
+        printArray(arr);
+    }
+}
