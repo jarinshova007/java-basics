e32b5f21b08b16c610929a2254624471587288dd
Your Name <your_email@example.com>
2026-02-18 13:52:01 +0600

    added

--- topic_06_arrays/Reverse.java
+++ topic_06_arrays/Reverse.java
@@ -2,7 +2,7 @@
 // Reverse an array
 import java.util.Scanner;
 
-public class ReverseAnArray {
+public class Reverse {
     // printArray method
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
