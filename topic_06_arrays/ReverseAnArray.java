fa2cbdd2db206de223f61e3adb78233aa7e0dedb
Your Name <your_email@example.com>
2026-02-18 13:31:36 +0600

    feat: add ReverseAnArray program

--- topic_06_arrays/ReverseAnArray.java
+++ topic_06_arrays/ReverseAnArray.java
@@ -2,7 +2,7 @@
 // Reverse an array
 import java.util.Scanner;
 
-public class ReverseArray {
+public class ReverseAnArray {
     // printArray method
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
