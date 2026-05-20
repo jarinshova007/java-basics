8407019518b15df8e5f223d64022fd92dafba2f3
Your Name <your_email@example.com>
2026-05-20 21:30:20 +0600

    modified

--- topic_05_functions/Average.java
+++ topic_05_functions/Average.java
@@ -3,22 +3,23 @@
 import java.util.Scanner;
 
 public class Average {
-    static int average(int n1, int n2, int n3) {
-        int avg = (n1 + n2 + n3) / 3;
+    static double average(double n1, double n2, double n3) {
+        double avg = (n1 + n2 + n3) / 3;
         return avg;
     }
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("num1 : ");
-    int num1 = sc.nextInt();
-    System.out.print("num2 : ");
-    int num2 = sc.nextInt();
-    System.out.print("num3 : ");
-    int num3 = sc.nextInt();
 
-    System.out.println("Average: " + average(num1, num2, num3));
-    
-    sc.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("num1 : ");
+        double num1 = sc.nextDouble();
+        System.out.print("num2 : ");
+        double num2 = sc.nextDouble();
+        System.out.print("num3 : ");
+        double num3 = sc.nextDouble();
+
+        System.out.printf("%.2f", average(num1, num2, num3));
+
+        sc.close();
 
     }
 }
